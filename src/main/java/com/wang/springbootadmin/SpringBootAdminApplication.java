f67e53a749a1eba2fc2c847e9fe9d9fc15686ba8
wangdd <iris19920918@163.com>
2019-01-03 19:37:10 +0800

    feat: 项目初始化

---
+++ src/main/java/com/wang/springbootadmin/SpringBootAdminApplication.java
@@ -0,0 +1,18 @@
+package com.wang.springbootadmin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@Configuration
+@EnableAdminServer
+public class SpringBootAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminApplication.class, args);
+    }
+
+}
+
